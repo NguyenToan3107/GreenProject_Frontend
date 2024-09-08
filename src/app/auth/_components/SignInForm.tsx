2d9a38b2227c365b52a35fcc2a3cdad21315d942
NguyenToan3107 <110925112+NguyenToan3107@users.noreply.github.com>
2024-09-08 22:34:37 +0700

    Responsive auth

--- src/app/auth/_components/SignInForm.tsx
+++ src/app/auth/_components/SignInForm.tsx
@@ -1,167 +1,165 @@
-"use client"
+"use client";
 import Image from "next/image";
-import {getUserInfo, loginRequest} from "@/apis/modules/auth";
-import React, {useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUser} from "@fortawesome/free-regular-svg-icons";
-import {faKey} from "@fortawesome/free-solid-svg-icons";
-import {toast} from "react-toastify";
-import {cookies} from "next/headers";
-import {setLocalStorage} from "@/ultis/localStorageUtils";
-
+import { getUserInfo, loginRequest } from "@/apis/modules/auth";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
+import { cookies } from "next/headers";
+import { setLocalStorage } from "@/ultis/localStorageUtils";
 
 interface SignInFormProps {
-    setPathname?: (value: (((prevState: string) => string) | string)) => void
+  setPathname?: (value: ((prevState: string) => string) | string) => void;
 }
 
-export default function SignInForm({setPathname}: SignInFormProps) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
+export default function SignInForm({ setPathname }: SignInFormProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    const login = async (event: any) => {
-        event.preventDefault();
-        setLoading(true);
+  const login = async (event: any) => {
+    event.preventDefault();
+    setLoading(true);
 
+    try {
+      const response: any = await loginRequest({
+        username: username,
+        password: password,
+      });
 
+      console.log(response);
 
-        try {
-            const response:any = await loginRequest({
-                username: username,
-                password: password,
-            });
+      if (response.code == 200) {
+        setLocalStorage("user_data", response.data);
+        const authorities: Array<string> = response.data.authorities;
+        console.log(authorities.at(0));
 
-            console.log(response);
-
-            if (response.code==200) {
-                setLocalStorage("user_data", response.data);
-                const authorities:Array<string>=response.data.authorities;
-                console.log(authorities.at(0))
-
-                if(authorities[0]=="ADMIN"){
-                     window.location.href="/admin"
-                }else if(authorities[0]=="USER") {
-                    window.location.href="/home"
-                }
-
-
-
-
-
-            }
-        } catch (error: any) {
-            console.log(error);
-            setError(error.response?.message || "Đăng nhập thất bại");
-        } finally {
-            setLoading(false);
+        if (authorities[0] == "ADMIN") {
+          window.location.href = "/admin";
+        } else if (authorities[0] == "USER") {
+          window.location.href = "/home";
         }
-    };
-
-    const handleUsernameChange = (event: any) => {
-        setUsername(event.target.value);
-    };
-
-    const handlePasswordChange = (event: any) => {
-        setPassword(event.target.value);
-    };
-
-    function loginWithGithub() {
-        window.location.href="http://localhost:7000/oauth2/authorization/github"
+      }
+    } catch (error: any) {
+      console.log(error);
+      setError(error.response?.message || "Đăng nhập thất bại");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    function loginWithGoogle() {
-        window.location.href="http://localhost:7000/oauth2/authorization/google"
-    }
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
 
-    return (
-        <div>
-            <form
-                className="mt-6 w-screen lg:flex-1 lg:w-full lg:px-0 px-7"
-                onSubmit={login}
-            >
-                <div className="mt-2 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faUser} className="mr-3 text-[18px]"/>
-                    <input
-                        type="text"
-                        placeholder="Tên đăng nhập"
-                        onChange={(e) => handleUsernameChange(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
 
-                <div className="mt-6 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]"/>
-                    <input
-                        type="password"
-                        placeholder="Mật khẩu"
-                        onChange={(e) => handlePasswordChange(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
+  function loginWithGithub() {
+    window.location.href = "http://localhost:7000/oauth2/authorization/github";
+  }
 
-                <p className="mt-5 text-xs cursor-pointer text-brand-blue">
-                    Quên mật khẩu
-                </p>
-                <button
-                    type="submit"
-                    className="mt-5 w-full bg-brand-primary text-white rounded-md py-2"
-                >
-                    {loading ? "Đăng nhập" : "Đăng nhập"}
-                </button>
-                {error && (
-                    <p className="mt-4 text-red-600 bg-red-100 border border-red-300 rounded-md p-2 text-sm">
-                        {error}
-                    </p>
-                )}
-            </form>
+  function loginWithGoogle() {
+    window.location.href = "http://localhost:7000/oauth2/authorization/google";
+  }
 
-            <div className="flex flex-col items-center w-full max-w-md">
-                <div className="mt-4 relative w-full mb-4">
-                    <div className="flex items-center justify-center">
-                        <div className="flex-grow border-t border-brand-gray"></div>
-                        <span className="mx-4 text-brand-gray whitespace-nowrap">HOẶC</span>
-                        <div className="flex-grow border-t border-brand-gray"></div>
-                    </div>
-                </div>
-
-                <div className="flex flex-row w-full max-w-md">
-                    <button  onClick={loginWithGoogle}
-                        className="flex-1 flex items-center justify-center border border-gray-300 bg-white text-black py-2 px-4 rounded-md mr-2">
-                        <Image
-                            className="rounded-xl mr-2"
-                            width={30}
-                            height={30}
-                            src="/images/google.png"
-                            alt="Google"
-                            quality={75}
-                        />
-                        Google
-                    </button>
-                    <button
-                        onClick={loginWithGithub}
-                        className="flex-1 flex items-center justify-center border border-gray-300 bg-white text-black py-2 px-4 rounded-md ml-2">
-                        <Image
-                            className="rounded-xl mr-2"
-                            width={30}
-                            height={30}
-                            src="/images/github.png"
-                            alt="GitHub"
-                            quality={75}
-                        />
-                        GitHub
-                    </button>
-                </div>
-            </div>
-
-            <p className="text-brand-gray text-center mt-5">
-                Bạn chưa có tài khoản?
-                <a onClick={()=> setPathname?.("/register") } className="text-brand-primary ml-2">
-                    Đăng ký
-                </a>
-            </p>
+  return (
+    <div className="flex flex-col items-center w-full max-w-lg mx-auto">
+      <form className="mt-6 w-full px-5 lg:px-0" onSubmit={login}>
+        <div className="mt-2 border-b border-gray-400 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faUser} className="mr-3 text-[18px]" />
+          <input
+            type="text"
+            placeholder="Tên đăng nhập"
+            onChange={(e) => handleUsernameChange(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
         </div>
-    );
+
+        <div className="mt-6 border-b border-gray-400 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]" />
+          <input
+            type="password"
+            placeholder="Mật khẩu"
+            onChange={(e) => handlePasswordChange(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
+        </div>
+
+        <p className="mt-5 text-xs cursor-pointer text-brand-blue">
+          Quên mật khẩu
+        </p>
+
+        <button
+          type="submit"
+          className="mt-5 w-full bg-brand-primary text-white rounded-md py-2"
+        >
+          {loading ? "Đăng nhập" : "Đăng nhập"}
+        </button>
+
+        {error && (
+          <p className="mt-4 text-red-600 bg-red-100 border border-red-300 rounded-md p-2 text-sm">
+            {error}
+          </p>
+        )}
+      </form>
+
+      <div className="flex flex-col items-center w-full">
+        <div className="mt-4 relative w-full mb-4">
+          <div className="flex items-center justify-center">
+            <div className="flex-grow border-t border-brand-gray"></div>
+            <span className="mx-4 text-brand-gray whitespace-nowrap">HOẶC</span>
+            <div className="flex-grow border-t border-brand-gray"></div>
+          </div>
+        </div>
+
+        <div className="flex flex-row w-full max-w-md">
+          <button
+            onClick={loginWithGoogle}
+            className="flex-1 flex items-center justify-center border border-gray-300 bg-white text-black py-2 px-4 rounded-md mr-2"
+          >
+            <Image
+              className="rounded-xl mr-2"
+              width={30}
+              height={30}
+              src="/images/google.png"
+              alt="Google"
+              quality={75}
+            />
+            Google
+          </button>
+
+          <button
+            onClick={loginWithGithub}
+            className="flex-1 flex items-center justify-center border border-gray-300 bg-white text-black py-2 px-4 rounded-md ml-2"
+          >
+            <Image
+              className="rounded-xl mr-2"
+              width={30}
+              height={30}
+              src="/images/github.png"
+              alt="GitHub"
+              quality={75}
+            />
+            GitHub
+          </button>
+        </div>
+      </div>
+
+      <p className="text-brand-gray text-center mt-5">
+        Bạn chưa có tài khoản?
+        <a
+          onClick={() => setPathname?.("/register")}
+          className="text-brand-primary ml-2 cursor-pointer"
+        >
+          Đăng ký
+        </a>
+      </p>
+    </div>
+  );
 }
--- src/app/auth/_components/SignUpForm.tsx
+++ src/app/auth/_components/SignUpForm.tsx
@@ -1,141 +1,141 @@
 "use client";
-import {redirect} from "next/navigation";
-import React, {useState} from "react";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { redirect } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUser} from "@fortawesome/free-regular-svg-icons";
-import {faKey} from "@fortawesome/free-solid-svg-icons";
-import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import Link from "next/link";
-import {registerRequest} from "@/apis/modules/auth";
-
+import { registerRequest } from "@/apis/modules/auth";
 
 interface SignUpFormProps {
-    setPathname?: (value: (((prevState: string) => string) | string)) => void
+  setPathname?: (value: ((prevState: string) => string) | string) => void;
 }
 
-export default function SignInForm({setPathname}: SignUpFormProps) {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
+export default function SignInForm({ setPathname }: SignUpFormProps) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    const register = async (event: any) => {
-        event.preventDefault();
-        setLoading(true);
+  const register = async (event: any) => {
+    event.preventDefault();
+    setLoading(true);
 
-        try {
-            const response = await registerRequest({
-                username: username,
-                email: email,
-                password: password,
-                passwordConfirm: passwordConfirm,
-            });
+    try {
+      const response = await registerRequest({
+        username: username,
+        email: email,
+        password: password,
+        passwordConfirm: passwordConfirm,
+      });
 
-            console.log(response);
-            if (response.status == 201) {
-
-                if (setPathname) {
-                    setPathname("/login")
-                }
-                toast.success(response.data.message);
-            }
-        } catch (error: any) {
-            console.log(error);
-            setError(error.response?.message || "Đăng ký thất bại");
-        } finally {
-            setLoading(false);
+      console.log(response);
+      if (response.status == 201) {
+        if (setPathname) {
+          setPathname("/login");
         }
-    };
+        toast.success(response.data.message);
+      }
+    } catch (error: any) {
+      console.log(error);
+      setError(error.response?.message || "Đăng ký thất bại");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const handleUsernameChange = (event: any) => {
-        setUsername(event.target.value);
-    };
-    const handleEmail = (event: any) => {
-        setEmail(event.target.value);
-    };
-    const handlePasswordChange = (event: any) => {
-        setPassword(event.target.value);
-    };
-    const handlePasswordConfirmChange = (event: any) => {
-        setPasswordConfirm(event.target.value);
-    };
-    return (
-        <div>
-            <form
-                className="mt-4 w-screen lg:flex-1 lg:w-full lg:px-0 px-7"
-                onSubmit={register}
-            >
-                <div className="mt-2 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faUser} className="mr-3 text-[18px]"/>
-                    <input
-                        type="text"
-                        placeholder="Họ và tên"
-                        onChange={(e) => handleUsernameChange(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
-
-                <div className="mt-6 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faEnvelope} className="mr-3 text-[18px]"/>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        onChange={(e) => handleEmail(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
-
-                <div className="mt-6 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]"/>
-                    <input
-                        type="password"
-                        placeholder="Mật khẩu"
-                        onChange={(e) => handlePasswordChange(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
-
-                <div className="mt-6 border-b border-gray pb-2 flex items-center px-3">
-                    <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]"/>
-                    <input
-                        type="password"
-                        placeholder="Nhập lại mật khẩu"
-                        onChange={(e) => handlePasswordConfirmChange(e)}
-                        required
-                        className="border border-none focus:outline-none text-base flex-1 py-[3px]"
-                    />
-                </div>
-
-                <p className="mt-5 text-xs cursor-pointer text-brand-blue">
-                    Quên mật khẩu
-                </p>
-                <button
-                    type="submit"
-                    className="mt-5 w-full bg-brand-primary text-white rounded-md py-2"
-                >
-                    Đăng ký
-                </button>
-                {error && (
-                    <p className="mt-4 text-red-600 bg-red-100 border border-red-300 rounded-md p-2 text-sm">
-                        {error}
-                    </p>
-                )}
-            </form>
-
-            <p className="text-brand-gray text-center mt-5">
-                Bạn đã có tài khoản?
-                <a className="text-brand-primary ml-2" onClick={()=> setPathname?.("/login") }>
-                    Đăng nhập
-                </a>
-            </p>
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
+  const handleEmail = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+  const handlePasswordConfirmChange = (event: any) => {
+    setPasswordConfirm(event.target.value);
+  };
+  return (
+    <div className="flex flex-col items-center w-full max-w-lg mx-auto px-4">
+      <form className="mt-4 w-full lg:px-0 px-4" onSubmit={register}>
+        <div className="mt-2 border-b border-gray-300 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faUser} className="mr-3 text-[18px]" />
+          <input
+            type="text"
+            placeholder="Họ và tên"
+            onChange={(e) => handleUsernameChange(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
         </div>
-    );
+
+        <div className="mt-6 border-b border-gray-300 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faEnvelope} className="mr-3 text-[18px]" />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => handleEmail(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
+        </div>
+
+        <div className="mt-6 border-b border-gray-300 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]" />
+          <input
+            type="password"
+            placeholder="Mật khẩu"
+            onChange={(e) => handlePasswordChange(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
+        </div>
+
+        <div className="mt-6 border-b border-gray-300 pb-2 flex items-center px-3">
+          <FontAwesomeIcon icon={faKey} className="mr-3 text-[18px]" />
+          <input
+            type="password"
+            placeholder="Nhập lại mật khẩu"
+            onChange={(e) => handlePasswordConfirmChange(e)}
+            required
+            className="border-none focus:outline-none text-base flex-1 py-[3px]"
+          />
+        </div>
+
+        <p className="mt-5 text-xs cursor-pointer text-brand-blue">
+          Quên mật khẩu
+        </p>
+
+        <button
+          type="submit"
+          className="mt-5 w-full bg-brand-primary text-white rounded-md py-2"
+        >
+          Đăng ký
+        </button>
+
+        {error && (
+          <p className="mt-4 text-red-600 bg-red-100 border border-red-300 rounded-md p-2 text-sm">
+            {error}
+          </p>
+        )}
+      </form>
+
+      <p className="text-brand-gray text-center mt-5">
+        Bạn đã có tài khoản?
+        <a
+          className="text-brand-primary ml-2 cursor-pointer"
+          onClick={() => setPathname?.("/login")}
+        >
+          Đăng nhập
+        </a>
+      </p>
+    </div>
+  );
 }
