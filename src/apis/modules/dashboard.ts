ae1a4ad85e0ffbcdf3f00e121f3a7c10918709b0
NguyenToan3107 <nguyentoan31072k3@gmail.com>
2024-10-15 23:09:02 +0700

    updeting dashboard

--- src/apis/modules/dashboard.ts
+++ src/apis/modules/dashboard.ts
@@ -2,3 +2,27 @@ import api from "@/apis/request";
 export function getDashBoard(){
     return api.get(`dashboard/statistics`);
 }
+
+export function getOrderDashBoard(){
+    return api.get(`dashboard/order`);
+}
+
+export function getRevenueDashBoard(){
+    return api.get(`dashboard/revenue`);
+}
+
+export function getUserDashBoard(quarter:number,year:number){
+    return api.get(`dashboard/user?quarter=${quarter}&year=${year}`);
+}
+
+export function getTopUserDashBoard(){
+    return api.get(`dashboard/top-user`);
+}
+
+export function getProductDashBoard(){
+    return api.get(`dashboard/products`);
+}
+
+export function getPercentageDashBoard(){
+    return api.get(`dashboard/percentage-returned-order`);
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,17 +2,21 @@
 import React, { useEffect, useState } from "react";
 import { Table, Spin } from "antd";
 import "antd/dist/reset.css"; // Import Ant Design styles
-import { getDashBoard } from "@/apis/modules/dashboard";
+import {
+  getDashBoard,
+  getRevenueDashBoard,
+  getUserDashBoard,
+} from "@/apis/modules/dashboard";
 
 export default function page() {
   const [loading, setLoading] = useState(false);
   const [dashboardData, setDashboardData] = useState<any>(null);
+  const [userData, setUserData] = useState<any>(null);
 
   const fetchDataDashBoard = async () => {
     setLoading(true);
     try {
       const res: any = await getDashBoard(); // Call API
-      console.log(res);
 
       if (res?.code === 200) {
         setDashboardData(res.data); // Lưu dữ liệu vào state
@@ -24,8 +28,38 @@ export default function page() {
     }
   };
 
+  const fetchUserDataDashBoard = async () => {
+    setLoading(true);
+    try {
+      const res: any = await getUserDashBoard(3, 2024); // Call API
+      if (res?.code === 200) {
+        setUserData(res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching dashboard data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchRevenueDashBoard = async () => {
+    setLoading(true);
+    try {
+      const res: any = await getRevenueDashBoard(); // Call API
+      console.log(res);
+      if (res?.code === 200) {
+      }
+    } catch (error) {
+      console.error("Error fetching dashboard data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     fetchDataDashBoard();
+    fetchUserDataDashBoard();
+    fetchRevenueDashBoard();
   }, []);
 
   if (loading) {
@@ -115,56 +149,51 @@ export default function page() {
   return (
     <div className="p-5 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-5 text-blue-600">Dashboard</h1>
-
       {/* Hiển thị các ô ngang hàng */}
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mb-6">
         <div className="cursor-pointer bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow rounded-lg p-4 text-center text-white">
-          <h2 className="text-xl font-semibold">Paid Orders</h2>
-          <p className="text-2xl font-bold">{dashboardData?.paidOrdersCount}</p>
-        </div>
-        <div className="cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 shadow rounded-lg p-4 text-center text-white">
-          <h2 className="text-xl font-semibold">New Users</h2>
-          <p className="text-2xl font-bold">{dashboardData.totalNewUser}</p>
-        </div>
-        <div className="cursor-pointer bg-gradient-to-r from-yellow-400 to-orange-500 shadow rounded-lg p-4 text-center text-white">
-          <h2 className="text-xl font-semibold">Total Order Value</h2>
+          <h2 className="text-xl font-semibold">Paid Orders Count</h2>
           <p className="text-2xl font-bold">
-            {dashboardData.totalPaidOrderValue}
+            {dashboardData?.paid_orders_count}
           </p>
         </div>
+        <div className="cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 shadow rounded-lg p-4 text-center text-white">
+          <h2 className="text-xl font-semibold">Tổng số voucher</h2>
+          <p className="text-2xl font-bold">{dashboardData.total_voucher}</p>
+        </div>
+
         <div className="cursor-pointer bg-gradient-to-r from-teal-400 to-cyan-500 shadow rounded-lg p-4 text-center text-white">
           <h2 className="text-xl font-semibold">Total Products</h2>
-          <p className="text-2xl font-bold">{dashboardData.totalProduct}</p>
+          <p className="text-2xl font-bold">{dashboardData.total_product}</p>
         </div>
         <div className="cursor-pointer bg-gradient-to-r from-indigo-400 to-purple-500 shadow rounded-lg p-4 text-center text-white">
           <h2 className="text-xl font-semibold">Total Users</h2>
-          <p className="text-2xl font-bold">{dashboardData.totalUser}</p>
+          <p className="text-2xl font-bold">{dashboardData.total_user}</p>
         </div>
       </div>
-
       <div className="grid grid-cols-3 md:grid-cols-10 gap-4">
         <div className="bg-white shadow rounded-lg p-4 col-span-3 md:col-span-4">
           <h2 className="text-xl font-semibold mb-4 text-blue-600">
             Top 10 người mua hàng hàng đầu
           </h2>
-          <Table
+          {/* <Table
             dataSource={dashboardData.Top10UserOrderByTotalOrderValue.content}
             columns={userColumns}
             rowKey="id"
-            pagination={false}
-          />
+            pagination={false} */}
+          {/* /> */}
         </div>
 
         <div className="bg-white shadow rounded-lg p-4 col-span-3 md:col-span-6">
           <h2 className="text-xl font-semibold mb-4 text-green-600">
             Sản phẩm bán chạy nhất
           </h2>
-          <Table
+          {/* <Table
             dataSource={dashboardData.bestSellerProducts.content}
             columns={productColumns}
             rowKey="id"
             pagination={false}
-          />
+          /> */}
         </div>
       </div>
     </div>
